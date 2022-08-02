1227ea7fd87c95dedc36e905e6b09ed519af6dcc
Shrushti Mane <shrusti@fathomable.in>
2022-08-02 22:20:12 +0530

    change4

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,43 @@ import ToogleDiv from "./components/ToogleDiv";
 import FormsInType from "./components/FormsInType";
 import CustomHookExample from "./components/CustomHookExample";
 import { LifecycleMethod, Information ,LifecycleMethodWithFunction} from "./components/LifeCycleMethods";
+import { ReactSEOMetaTags } from 'react-seo-meta-tags'
+const siteMetadata={
+  url: 'https://google.com/about',
+  title:  'This is a 70 character long title with a lot of padding to make it so!',
+  datePublished: '2019-10-06T13:56:03.123Z',
+  description: 'This is a 200 character long description of this web page which is quite interesting and which describes its contents well with a lot of relevant keywords and isn\'t just general marketing mumbo-jumbo.',
+  language: 'en-US',
+  image: 'https://miro.medium.com/max/640/1*0ZhzN-2EUXmov3HHwOjKCQ.pnghttp://website.com/image.png',
+  author: {
+    email: 'person@gmail.com',
+    name: 'John Smith',
+    image: 'https://miro.medium.com/max/640/1*0ZhzN-2EUXmov3HHwOjKCQ.png',
+  },
+  site: {
+    siteName: 'IMDb',
+    searchUrl: 'https://www.google.com/search?q=',
+  }
+}
 function App() {
   return (
     <div className="App">
+      <ReactSEOMetaTags
+      website={{ ...siteMetadata }}
+      // breadcrumb={[
+      //   { name: 'My Site', url: 'https://me.com' },
+      //   { name: 'Blog', url: 'https://me.com/blog' },
+      // ]}
+      organization={{
+        name: 'Google',
+        legalName: 'Google Inc',
+        url: 'https://google.com',
+        logo: 'https://google.com/logo.jpg'
+      }}
+      // blogPost={{ ...blogPost }}
+      // facebook={{ facebookAppId: 'abc123' }}
+      // twitter={{ twitterUser: '@mickey_mouse' }}
+    />
       <header className="App-header">
       <ToogleDiv/>
       <FormsInType/>
--- src/components/ToogleDiv.tsx
+++ src/components/ToogleDiv.tsx
@@ -1,4 +1,27 @@
 import React,{useState} from 'react'
+import {
+  EmailIcon,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  HatenaIcon,
+  InstapaperIcon,
+  LineIcon,
+  LinkedinIcon,
+  LivejournalIcon,
+  MailruIcon,
+  OKIcon,
+  PinterestIcon,
+  PocketIcon,
+  RedditIcon,
+  TelegramIcon,
+  TumblrIcon,
+  TwitterIcon,
+  ViberIcon,
+  VKIcon,
+  WeiboIcon,
+  WhatsappIcon,
+  WorkplaceIcon,FacebookShareButton,TwitterShareButton
+} from "react-share";
 
 export default function ToogleDiv() {
     const [isToogle,setToogle] = useState(true);
@@ -6,6 +29,23 @@ export default function ToogleDiv() {
     <div style={{display:"flex"}}>ToogleDiv
         <input type="checkbox" checked={isToogle} onChange={(e:any) => setToogle(isToogle ? false : true) } />
        {isToogle ? <h2>my Name is  SHRUSHTI</h2> : null } 
+
+       <FacebookShareButton
+            url={"https://typescript-demo-react.herokuapp.com/"}
+            quote={"Demo2"}
+            className="Demo__some-network__share-button"
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+
+          <TwitterShareButton
+            url={"https://typescript-demo-react.herokuapp.com/"}
+            title={"Demo1"}
+            className="Demo__some-network__share-button"
+          >
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+
     </div>
   )
 }
